240130a4c83f803dd3ac3b390b00f75f365c9b73
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-03-14 09:04:49 -0700

    add express.json

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ app.listen(3000, () => {
 });
 //pass the index.html that is client side file to the server.
 app.use(express.static('public'));
+
+// GET method route
+// app.get('/', function(req, res) {
+//   res.send('GET request to the homepage');
+// });
+app.use(express.json({ limit: '1mb' }));
+// POST method route
+app.post('/api', (req, res) => {
+  console.log(req.body);
+  res.send('POST request to the homepage');
+});
