62792a6006fa894352deddc6a6866475922dd0c9
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-03-14 11:20:02 -0700

    change icon and add style

---
+++ public/script.js
@@ -0,0 +1,50 @@
+//map (leaflet)
+// let lat;
+// let lon;
+const map = L.map('map').setView([0, 0], 3);
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tiles = L.tileLayer(tileURL, { attribution });
+tiles.addTo(map);
+const myIcon = L.icon({
+  iconUrl: './cute.png',
+
+  iconSize: [50, 50] // size of the icon
+
+  // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  // shadowAnchor: [4, 62], // the same for the shadow
+  //popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+const marker = L.marker([0, 0], { icon: myIcon })
+  .bindPopup('<b>Hello world!</b><br>I am here.')
+  .openPopup()
+  .addTo(map);
+
+const getLoc = () => {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(async position => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      marker.setLatLng([lat, lon]);
+      map.setView([lat, lon], 13);
+      document.getElementById('lat').innerHTML = lat;
+      document.getElementById('lon').innerHTML = lon;
+
+      const data = { lat, lon };
+      const option = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      };
+      const response = await fetch('./api', option);
+      const json = await response.json();
+      console.log(json);
+    });
+  } else {
+    console.log('geolocation IS NOT available');
+  }
+};
+getLoc();
